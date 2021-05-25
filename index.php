b98fcaa32c706c96ed738a09c45defe140d3fc2e
dev3 <tiago.inovatech.inf@gmail.com>
2021-05-25 11:05:48 -0300

    Theory time - selectors

--- index.php
+++ index.php
@@ -6,16 +6,18 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>uHost</title>
     <link rel="shortcut icon" href="favicon.png">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Anton&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="main.css">
 </head>
 
 <body>
     <main>
-        <section>
+        <section id="product-overview">
             <h1>Get the freedom you deserve.</h1>
         </section>
-        <section>
-            <h1>Choose your plan</h1>
+        <section id="plans">
+            <h1 class="section-title">Choose your plan</h1>
         </section>
     </main>
 </body>
