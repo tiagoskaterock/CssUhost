ef455f6574fd3eadcb579faa5f16ae05692ffecf
tiago <tiagolemespalhano@gmail.com>
2021-05-26 12:28:15 -0300

    navbar started

---
+++ includes/header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>uHost</title>
+  <link rel="shortcut icon" href="favicon.png">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Anton&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="main.css">
+</head>
+
+<body>
+	<header class="main-header">
+		<div>
+			<a href="index.php">
+				uHost
+			</a>
+		</div>
+
+		<nav class="main-nav">
+			<ul class="main-nav__items">
+				<li class="main-nav__item">
+					<a href="#">Packges</a>
+				</li>
+				<li class="main-nav__item">
+					<a href="#">Customers</a>
+				</li>
+				<li class="main-nav__item">
+					<a href="#">Start Hosting</a>
+				</li>
+			</ul>
+		</nav>		
+	</header>
--- index.php
+++ index.php
@@ -1,25 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php include('includes/header.php') ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>uHost</title>
-    <link rel="shortcut icon" href="favicon.png">
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Anton&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="main.css">
-</head>
-
-<body>
-    <main>
-        <section id="product-overview">
-            <h1>Get the freedom you deserve.</h1>
-        </section>
-        <section id="plans">
-            <h1 class="section-title">Choose your plan</h1>
-        </section>
-    </main>
+  <main>
+    <section id="product-overview">
+      <h1>Get the freedom you deserve.</h1>
+    </section>
+    <section id="plans">
+      <h1 class="section-title">Choose your plan</h1>
+      <p>Make sure to get the most for your money!</p>
+    </section>
+  </main>
 </body>
+
+<footer>
+  
+</footer>
 
 </html>
