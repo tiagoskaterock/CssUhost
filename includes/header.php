1722f7a3a993292935e56f2ec4d342a2a87f0f95
tiago <tiagolemespalhano@gmail.com>
2021-05-27 21:16:40 -0300

    Adding pesudo classes

--- includes/header.php
+++ includes/header.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>uHost</title>
+  <title>Tiago Developer - CSS</title>
   <link rel="shortcut icon" href="favicon.png">
   <link rel="preconnect" href="https://fonts.gstatic.com">
   <link href="https://fonts.googleapis.com/css2?family=Anton&display=swap" rel="stylesheet">
@@ -15,20 +15,20 @@
 	<header class="main-header">
 		<div>
 			<a href="index.php" class="main-header__brand">
-				uHost
+				Tiago Dev
 			</a>
 		</div>
 
 		<nav class="main-nav">
 			<ul class="main-nav__items">
 				<li class="main-nav__item">
-					<a href="#">Packges</a>
+					<a href="#">Front End</a>
 				</li>
 				<li class="main-nav__item">
-					<a href="#">Customers</a>
+					<a href="#">Back End</a>
 				</li>
 				<li class="main-nav__item">
-					<a href="#">Start Hosting</a>
+					<a href="#">Full Stack</a>
 				</li>
 			</ul>
 		</nav>		
