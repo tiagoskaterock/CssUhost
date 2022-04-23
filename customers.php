fd0b54c818821dff01870dfece20066f52c6ff1b
tiago <tiagolemespalhano@gmail.com>
2022-04-22 22:26:49 -0300

    Customres images changed

--- customers.php
+++ customers.php
@@ -4,10 +4,10 @@
         <div>
             <div class="testimonial" id="customer-1">
                 <div class="testimonial__image-container">
-                    <img src="images/customer-1.jpg" alt="Mike Statham - Customer" class="testimonial__image">
+                    <img src="images/1.jpeg" alt="Mike Statham - Customer" class="testimonial__image">
                 </div>
                 <div class="testimonial__info">
-                    <h1 class="testimonial__name">Mike Statham</h1>
+                    <h1 class="testimonial__name">Jane Statham</h1>
                     <h2 class="testimonial__subtitle">Founder of
                         <a href="tech-analysis.com">tech-analysis.com</a>
                     </h2>
@@ -17,7 +17,7 @@
             <div class="testimonial" id="customer-2">
 
                 <div class="testimonial__info">
-                    <h1 class="testimonial__name">John Mellow</h1>
+                    <h1 class="testimonial__name">Mary Mellow</h1>
                     <h2 class="testimonial__subtitle">Hosts his private videos on uHost.
                     </h2>
                     <p class="testimonial__text">I worked as a blogger and always looked for an integrated hosting and file storage solution. I found
@@ -25,7 +25,7 @@
                     </p>
                 </div>
                 <div class="testimonial__image-container">
-                    <img src="images/customer-2.jpg" alt="John Mellow - Customer" class="testimonial__image">
+                    <img src="images/2.jpg" alt="John Mellow - Customer" class="testimonial__image">
                 </div>
             </div>
         </div>
